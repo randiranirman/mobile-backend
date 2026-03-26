8a35a5f83989c8b87f96420a5c1ee0a8bf1ca28d
randiranirman <randiranirman20011210@gmail.com>
2026-03-26 23:24:37 +0530

    modified model with realtions

--- src/main/java/org/devops/mobileshop/model/Order.java
+++ src/main/java/org/devops/mobileshop/model/Order.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Document( collection = "orders")
 @Data
@@ -18,5 +22,24 @@ public class Order {
 
     @Id
     private String id ;
+
+    @DBRef
+    private User user ;
+
+    @DBRef
+    private List<Product> productList;
+
+    @DBRef
+    private Deliver deliver;
+
+
+    private double totalAmount ;
+    private String status ; // pending , delivered , canceld
+
+    private String  shippingAddress ;
+
+
+
+
 
 }
--- src/main/java/org/devops/mobileshop/model/Product.java
+++ src/main/java/org/devops/mobileshop/model/Product.java
@@ -1,30 +1,32 @@
-package org.devops.mobileshop.model;
+    package org.devops.mobileshop.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.bson.codecs.pojo.annotations.BsonId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+    import lombok.AllArgsConstructor;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+    import org.bson.codecs.pojo.annotations.BsonId;
+    import org.springframework.data.annotation.Id;
+    import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document( collection = "products")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+    @Document( collection = "products")
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
 
-public class Product {
+    public class Product {
 
 
-    @Id
-    private String id ;
+        @Id
+        private String id ;
 
-    private String  model ;
+        private String  model ;
 
-    private double price ;
-    private int quantity ;
-    private  int stock ;
-    private String description ;
-    private String image ;
+        private double price ;
+        private int quantity ;
+        private  int stock ;
+        private String description ;
+        private String image ;
 
-}
+
+
+    }
